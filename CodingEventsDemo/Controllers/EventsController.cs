7f275de3f02768adb73d93a1f4f08974b1819212
Tom Hang <tbhang31@gmail.com>
2022-05-10 11:39:50 -0400

    We're only on 14.2 but pushing

--- CodingEventsDemo/Controllers/EventsController.cs
+++ CodingEventsDemo/Controllers/EventsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodingEventsDemo.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,17 +11,33 @@ namespace coding_events_practice.Controllers
 {
     public class EventsController : Controller
     {
+        static private List<Event> Events = new List<Event>();
+
         // GET: /<controller>/
+        [HttpGet]
         public IActionResult Index()
         {
-            List<string> Events = new List<string>();
-            Events.Add("Code With Pride");
-            Events.Add("Apple WWDC");
-            Events.Add("Strange Loop");
-
             ViewBag.events = Events;
 
             return View();
         }
+
+        [HttpGet]
+        public IActionResult Adds()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [Route("/Events/Adds")]
+        public IActionResult NewEvent (string name, string desc)
+        {
+            Events.Add(new Event(name, desc));
+
+            return Redirect("/Events");
+        }
+
     }
+
+
 }
